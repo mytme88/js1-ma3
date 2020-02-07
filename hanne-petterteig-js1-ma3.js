51b40fd33924d5de645d9ec183593bbf1b96fd03
Hanne Petterteig <mytme88@gmail.com>
2020-02-07 19:22:31 +0100

    Final

---
+++ hanne-petterteig-js1-ma3.js
@@ -0,0 +1,64 @@
+// Question 1
+const sum = (a, b) => a - b;
+console.log(sum(10, 5));
+
+// Question 2
+fetch("https://api.rawg.io/api/games?genres=sports")
+  .then(response => {
+    return response.json();
+  })
+  .then(gamesJSON => {
+    const games = gamesJSON.results;
+    games.forEach(game => console.log(game.name));
+  })
+  .catch(() => {
+    document.location.href = "/error.html";
+  });
+
+// Question 3
+const p = "These cats are outrageous.";
+console.log(p.replace("cats", "giraffes"));
+
+// Question 4
+// Assuming our location is https://my.site.com?userId=7
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+let userId = params.get("userId");
+
+/*if (!userId) {
+  document.location.href = "/third.html";
+} else {
+  userId = parseInt(userId, 10);
+  if (userId < 10) {
+    document.location.href = "/first.html";
+  } else if (userId >= 10) {
+    document.location.href = "/second.html";
+  }
+}*/
+
+// Question 5
+const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
+container.removeChild(btn);
+
+// Question 6
+const parrots = document.createElement("li");
+const textnode = document.createTextNode("Parrots");
+parrots.className = "parrots";
+parrots.appendChild(textnode);
+const animalList = document.querySelector(".animals");
+animalList.insertBefore(parrots, animalList.childNodes[2]);
+
+// Question 7
+const ratingDiv = document.querySelector(".rating");
+fetch("https://api.rawg.io/api/games/3801")
+  .then(response => {
+    return response.json();
+  })
+  .then(gamesJSON => {
+    const rating = gamesJSON.rating;
+    ratingDiv.innerText = `Rating: ${rating}`;
+  })
+  .catch(error => {
+    console.error("Encountered error:", error);
+  });
